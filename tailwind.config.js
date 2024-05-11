9fc6b9e6ed036a568086dd73bd27722eedb74761
sarina <sarinaem75@gmail.com>
2024-05-12 00:46:18 +0330

    add sildebar and nav in project

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,12 @@ module.exports = {
         sans: ["IranSans"],
       },
     },
+    
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+  ],
 };
